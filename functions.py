6e5b9b22195ff37c7dfb43886738a5f6c81e27b8
Nishant12322 <nishantaryal12@gmail.com>
2023-11-07 15:33:40 +0545

    initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+#functions
+#def my_function():
+#  print("Hello from a function")
+
+#my_function()
+#
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Email")
+my_function("ContactNo")
+my_function("Address")
